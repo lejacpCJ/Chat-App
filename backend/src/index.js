3617d7fc6fa5d0067260bca7d56b40c11482e713
CJ Wang <lejacp@gmail.com>
2025-10-07 23:36:44 +0800

    implement add profilePic and checkAuth

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParse from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -7,8 +8,10 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParse());
 
 app.use("/api/auth", authRoutes);
 
